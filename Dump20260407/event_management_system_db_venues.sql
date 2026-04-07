77678f10f50b96ae784eaf21c315d773c1c76ae5
villafuerteguanzon-creator <villafuerteguanzon@gmail.com>
2026-04-07 17:39:12 +0800

    Add files via upload

---
+++ Dump20260407/event_management_system_db_venues.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: event_management_system_db
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `venues`
+--
+
+LOCK TABLES `venues` WRITE;
+/*!40000 ALTER TABLE `venues` DISABLE KEYS */;
+INSERT INTO `venues` VALUES (1000,'Grand Convention Center','123 Tech Street, Cebu City',500),(1001,'Indio, California, USA','Indio, California, USA',10000),(1002,'Pilton, England, UK','Pilton, England, UK',10000),(1003,'Chicago, Illinois, USA','Chicago, Illinois, USA',10000),(1004,'Austin, Texas, USA','Austin, Texas, USA',10000),(1005,'Boom, Belgium','Boom, Belgium',10000),(1006,'Las Vegas, Nevada, USA','Las Vegas, Nevada, USA',10000),(1007,'Miami, Florida, USA','Miami, Florida, USA',10000),(1008,'Tokyo, Japan','Tokyo, Japan',10000),(1009,'Roskilde, Denmark','Roskilde, Denmark',10000),(1010,'Atlanta, Georgia, USA','Atlanta, Georgia, USA',10000),(1011,'Las Vegas, USA','Las Vegas, USA',10000),(1012,'Reading, England, UK','Reading, England, UK',10000),(1013,'Leeds, England, UK','Leeds, England, UK',10000),(1014,'Zilker Park, Austin, USA','Zilker Park, Austin, USA',10000),(1015,'São Paulo, Brazil','São Paulo, Brazil',10000),(1016,'Kalibo, Aklan','Kalibo, Aklan',10000),(1017,'Cebu City','Cebu City',10000),(1018,'Lucban, Quezon','Lucban, Quezon',10000),(1019,'Baguio City','Baguio City',10000),(1020,'Bacolod City','Bacolod City',10000),(1021,'Madrid, Spain','Madrid, Spain',10000),(1022,'Barcelona, Spain','Barcelona, Spain',10000),(1023,'Manchester, UK','Manchester, UK',10000),(1024,'Nevada, USA','Nevada, USA',10000),(1025,'Split, Croatia','Split, Croatia',10000),(1026,'Scheeßel, Germany','Scheeßel, Germany',10000),(1027,'Neuhausen ob Eck, Germany','Neuhausen ob Eck, Germany',10000),(1028,'Budapest, Hungary','Budapest, Hungary',10000),(1029,'Paris, France','Paris, France',10000),(1030,'Munich, Germany','Munich, Germany',10000),(1031,'Sydney, Australia','Sydney, Australia',10000),(1032,'Rio de Janeiro, Brazil','Rio de Janeiro, Brazil',10000),(1033,'Venice, Italy','Venice, Italy',10000),(1034,'Bangkok, Thailand','Bangkok, Thailand',10000),(1035,'Dublin, Ireland','Dublin, Ireland',10000),(1036,'USA (nationwide)','USA (nationwide)',10000),(1037,'Worldwide','Worldwide',10000),(1038,'Pamplona, Spain','Pamplona, Spain',10000),(1039,'Amsterdam, Netherlands','Amsterdam, Netherlands',10000),(1040,'Notting Hill, London','Notting Hill, London',10000),(1041,'UK','UK',10000),(1042,'USA','USA',10000);
+/*!40000 ALTER TABLE `venues` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-07 17:38:34
